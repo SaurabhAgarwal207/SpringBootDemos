84d6b9ae19df12c89dc33fdeafd096864c525ee0
SaurabhAgarwal207 <mcasaurabhagarwal@gmail.com>
2021-01-09 20:57:38 +0530

    Master Commit, Project to learn how to create proj from spring initializr https://start.spring.io/

---
+++ StudentSpringBootApp/src/main/java/com/saurabh/app/StudentSpringBootApp/controller/StudentController.java
@@ -0,0 +1,16 @@
+package com.saurabh.app.StudentSpringBootApp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController {
+
+	@GetMapping(value="/")
+	//@RequestMapping(value="/",method=RequestMethod.GET)
+	public String hello() {
+		return " Hello World Students !!!";
+	}
+}
---
+++ StudentSpringBootApp/src/test/java/com/saurabh/app/StudentSpringBootApp/StudentSpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saurabh.app.StudentSpringBootApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentSpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
